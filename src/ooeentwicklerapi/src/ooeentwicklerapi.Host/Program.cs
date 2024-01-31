3ed60d0467df8b4cf8074c1f809201d482007c87
Daniel Hufnagl <d.hufnagl@codelisk.com>
2024-01-31 19:01:56 +0100

    Maby finished setup

--- src/ooeentwicklerapi/src/ooeentwicklerapi.Host/Program.cs
+++ src/ooeentwicklerapi/src/ooeentwicklerapi.Host/Program.cs
@@ -1,6 +1,7 @@
 
 using Framework.Restservice.Database;
 using Framework.Restservice.Server;
+using Microsoft.EntityFrameworkCore;
 using ooeentwicklerapi.Database;
 using ooeentwicklerapi.Foundation;
 
@@ -22,6 +23,7 @@ namespace ooeentwicklerapi.Host
         {
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+            builder.Services.AddTransient<DbContext, OoeDbContext>();
             builder.Services.AddDbContext<OoeDbContext>(opt =>
             {
                 //opt.UseMySql(connectionString, new MariaDbServerVersion("15.1"), x => x.MigrationsAssembly("Backend.Host"));
