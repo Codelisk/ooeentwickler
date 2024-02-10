ec426c7c86c0684d721693bb94bc439e71f3bb89
Daniel Hufnagl <d.hufnagl@codelisk.com>
2024-02-10 20:05:27 +0100

    Update

--- src/ooeentwickleruno/ooeentwickleruno/Presentation/BodyView.cs
+++ src/ooeentwickleruno/ooeentwickleruno/Presentation/BodyView.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Framework.UnoNative.Views.Pages;
+using Microsoft.UI.Xaml.Media.Imaging;
 using ooeentwickleruno.controls.Buttons;
 
 namespace Sample.Presentation;
@@ -15,26 +16,29 @@ public partial class BodyView : RegionBasePage<BodyViewModel>
     protected override UIElement MainContent(BodyViewModel vm)
     {
         return new Grid().Children(
-            new CardContentControl()
-                .Style(StaticResource.Get<Style>("ElevatedCardContentControlStyle"))
-                .Width(300)
-                .Height(250)
-                .Content(
-                    new Grid().Children(
-                        new Image()
-                            .Source(
-                                "https://www.simpleimageresizer.com/_uploads/photos/e6cd4d6c/kateryna-ivasiva-86CYiW-WP1g-unsplash_400x300.jpg"
+            new ItemsRepeater()
+                .ItemsSource(() => vm.Districts)
+                .ItemTemplate<(DistrictDto, BitmapImage)>(x =>
+                {
+                    return new CardContentControl()
+                        .Style(StaticResource.Get<Style>("ElevatedCardContentControlStyle"))
+                        .Width(300)
+                        .Height(250)
+                        .Content(
+                            new Grid().Children(
+                                new Image()
+                                    .Source(() => x.Item2)
+                                    .VerticalAlignment(VerticalAlignment.Center)
+                                    .HorizontalAlignment(HorizontalAlignment.Center)
+                                    .Stretch(Stretch.UniformToFill),
+                                new ElevatedButton()
+                                    .Content(() => x.Item1)
+                                    .Command(x => x.Bind(() => vm.NavigateCommand))
+                                    .VerticalAlignment(VerticalAlignment.Bottom)
+                                    .Margin(10)
                             )
-                            .VerticalAlignment(VerticalAlignment.Center)
-                            .HorizontalAlignment(HorizontalAlignment.Center)
-                            .Stretch(Stretch.UniformToFill),
-                        new ElevatedButton()
-                            .Content(x => x.Bind(() => vm.Test))
-                            .Command(x => x.Bind(() => vm.NavigateCommand))
-                            .VerticalAlignment(VerticalAlignment.Bottom)
-                            .Margin(10)
-                    )
-                )
+                        );
+                })
         );
     }
 }
--- src/ooeentwickleruno/ooeentwickleruno/Presentation/BodyViewModel.cs
+++ src/ooeentwickleruno/ooeentwickleruno/Presentation/BodyViewModel.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using DynamicData;
 using Framework.Mvvm.ViewModels;
 using Framework.Services.Services.Vms;
+using Microsoft.UI.Xaml.Media.Imaging;
 using ooeentwickleruno.apiclient;
 using ReactiveUI;
 
@@ -14,15 +16,57 @@ namespace Sample.Presentation;
 public partial class BodyViewModel : RegionBaseViewModel
 {
     private string test = "Test";
+    private readonly IDistrictImageRepository _districtImageRepository;
+    private readonly IDistrictRepository _districtRepository;
+
     public string Test
     {
         get { return test; }
         set { this.RaiseAndSetIfChanged(ref test, value); }
     }
 
-    public BodyViewModel(VmServices vmServices)
-        : base(vmServices) { }
+    public BodyViewModel(
+        VmServices vmServices,
+        IDistrictImageRepository districtImageRepository,
+        IDistrictRepository districtRepository
+    )
+        : base(vmServices)
+    {
+        _districtImageRepository = districtImageRepository;
+        _districtRepository = districtRepository;
+    }
 
+    public override void OnNavigatedTo(NavigationContext navigationContext)
+    {
+        base.OnNavigatedTo(navigationContext);
+        _districtRepository
+            .GetAll()
+            .ContinueWith(x =>
+            {
+                foreach (var district in x.Result)
+                {
+                    _districtImageRepository
+                        .Get(district.GetId())
+                        .ContinueWith(y =>
+                        {
+                            if (y.Result == null)
+                            {
+                                Districts.Add((district, null));
+                            }
+                            else
+                            {
+                                var image = new BitmapImage();
+                                image.SetSource(new MemoryStream(y.Result.bytes));
+                                Districts.Add((district, image));
+                            }
+                        });
+                }
+                this.RaisePropertyChanged(nameof(Districts));
+            });
+    }
+
+    public List<(DistrictDto, BitmapImage)> Districts { get; set; } =
+        new List<(DistrictDto, BitmapImage)>();
     public ICommand NavigateCommand => this.LoadingCommand(OnNavigateAsync);
 
     private async Task OnNavigateAsync()
