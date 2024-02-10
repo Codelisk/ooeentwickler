096625b9028cbfcff93386e1a2ae890ccdcea03a
Daniel Hufnagl <d.hufnagl@codelisk.com>
2024-02-11 00:06:34 +0100

    Updates

--- src/ooeentwickleruno/Modules/ooeentwickleruno.controls/Buttons/Fab/DefaultFab.cs
+++ src/ooeentwickleruno/Modules/ooeentwickleruno.controls/Buttons/Fab/DefaultFab.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Uno.Material;
+using Uno.Themes;
 
 namespace ooeentwickleruno.controls.Buttons.Fab;
+
 public partial class DefaultFab : Button
 {
     public DefaultFab()
     {
         this.Style(StaticResource.Get<Style>("SmallFabStyle"));
-        this.ControlExtensions(icon: new SymbolIcon()
-                .Symbol(Symbol.Add));
+        this.ControlExtensions(icon: new SymbolIcon().Symbol(Symbol.Add));
     }
 }
