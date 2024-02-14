6243db121bec040830a6c402102375852b0f65ba
Daniel Hufnagl <d.hufnagl@codelisk.com>
2024-02-14 10:13:37 +0100

    Updates

---
+++ build/BuildApi/MainModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModularPipelines.Context;
+using ModularPipelines.DotNet.Extensions;
+using ModularPipelines.Git.Extensions;
+using ModularPipelines.GitHub.Extensions;
+using ModularPipelines.Modules;
+
+namespace BuildApi
+{
+    public class MainModule : Module<string>
+    {
+        protected override async Task<string?> ExecuteAsync(
+            IPipelineContext context,
+            CancellationToken cancellationToken
+        )
+        {
+            var test = context.Git();
+            context.DotNet().Publish(new()
+            {
+                
+            })
+            return null;
+        }
+    }
+}
---
+++ build/BuildApi/Program.cs
@@ -0,0 +1,22 @@
+﻿using BuildBase.Configs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ModularPipelines.Extensions;
+using ModularPipelines.Host;
+
+namespace BuildApi
+{
+    internal class Program
+    {
+        static async Task DoAsync()
+        {
+            await PipelineHostBuilder.Create().AddModule<MainModule>().ExecutePipelineAsync();
+        }
+
+        static void Main(string[] args)
+        {
+            DoAsync().Wait();
+        }
+    }
+}
