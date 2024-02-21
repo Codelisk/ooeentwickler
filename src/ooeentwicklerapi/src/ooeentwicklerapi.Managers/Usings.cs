8152b1730d6b770f7120b822983832db41438bd9
Daniel Hufnagl <d.hufnagl@codelisk.com>
2024-02-21 09:50:14 +0100

    update

--- src/ooeentwicklerapi/src/ooeentwicklerapi.Managers/Usings.cs
+++ src/ooeentwicklerapi/src/ooeentwicklerapi.Managers/Usings.cs
@@ -1,1 +1,6 @@
 ﻿global using ooeentwicklerapi.Repositories;
+global using Framework.Restservice.Managers.Base;
+global using Codelisk.GeneratorAttributes.WebAttributes.HttpMethod;
+global using Codelisk.GeneratorAttributes.WebAttributes.Manager;
+global using Framework.Restservice.Repositories.Base;
+global using Framework.Restservice.Repositories;
